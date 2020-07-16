d2fdd5529c801634f60e4feb09162ddd1340bb89
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-16 17:18:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,6 +23,13 @@ class Song
     @@all[-1]
   end
 
+  def find_by_name(name)
+    @@all.each do |song|
+      return song if song.name = name
+    end
+    return nil
+  end
+
   def save
     self.class.all << self
   end
